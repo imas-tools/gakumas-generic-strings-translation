83c4e9ccd2834377589a72ac4d6b4d8b0b6614d0
Badmingrams <161575566+Badmingrams@users.noreply.github.com>
2024-07-15 23:15:32 +0800

    feat: remove empty files when arranging

--- scripts/arrange.py
+++ scripts/arrange.py
@@ -1,11 +1,13 @@
 import os, json
+from typing import Union, Dict
 from utils import get_kvs_for_dir, translation_dir, source_strings_dir, yield_string
 
 
-def arrange():
-    all_kvs = get_kvs_for_dir(translation_dir)
+def arrange(all_kvs: Union[None, Dict[str, str]] = None):
+    if all_kvs is None:
+        all_kvs = get_kvs_for_dir(translation_dir)
     # filename -> {key -> value}
-    kvs_to_update: dict[str, dict[str, str]] = {}
+    kvs_to_update: Dict[str, Dict[str, str]] = {}
 
     # update
     # init empty kvs_to_update filename
@@ -15,7 +17,7 @@ def arrange():
                 continue
             file_path = os.path.join(root_path, file)
             kvs_to_update[file_path] = {}
-            
+
     # arrange kvs
     for root_path, _, files in os.walk(source_strings_dir):
         for file in files:
@@ -25,14 +27,17 @@ def arrange():
             rel_path = os.path.relpath(file_path, source_strings_dir)
             dest_file_path = os.path.join(translation_dir, rel_path)
             kvs_to_update.setdefault(dest_file_path, {})
-                
+
             for string, _ in yield_string(file_path):
                 if string not in all_kvs:
                     continue
                 kvs_to_update[dest_file_path][string] = all_kvs[string]
-    
+
     # write kvs
     for filename, kvs in kvs_to_update.items():
+        if len(kvs.values()) == 0:
+            os.remove(filename)
+            continue
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, "w", encoding="utf-8") as f:
             json.dump(kvs, f, ensure_ascii=False, indent=4)
